a758d71300a167f5ddc58cd979ac5764a8f91714
RUIZ1200 <aruimar2004@gmail.com>
2024-03-11 13:38:13 +0100

    terminado

--- P04-Redes/Ejercicio_03.cpp
+++ P04-Redes/Ejercicio_03.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "netinet/in.h"
 
 using namespace std;
 
 int main(){
-    
+    short num16=65;
+    long num32=429;
+
+    cout << ntohs(num16) << "\n" << ntohs(num32) << "\n" << ntohl(num16) << "\n" << ntohl(num32) << "\n\n";
+    cout << htons(num16) << "\n" << htons(num32) << "\n" << htonl(num16) << "\n" << htonl(num32) << "\n";
+
+    return 0;
 }
